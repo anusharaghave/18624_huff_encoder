73d55eed78fecb45404533e4886d2d5787aeaf83
anusharaghave <98190979+anusharaghave@users.noreply.github.com>
2023-04-16 17:08:01 -0400

    io changes

--- src/huff_enc.v
+++ src/huff_enc.v
@@ -1,31 +1,32 @@
 module huff_encoder (
 	clk,
 	reset,
-	data_in,
-	freq_in,
-	encoded_value,
-	encoded_mask,
-	done
+	io_in,
+	io_out
 );
 	input wire clk;
 	input wire reset;
-	input wire [23:0] data_in;
-	input wire [8:0] freq_in;
-	output reg [8:0] encoded_value;
-	output reg [8:0] encoded_mask;
-	output reg done;
-	reg [20:0] character;
+	input wire [11:0] io_in;
+	output reg [11:0] io_out;
+	reg [23:0] data_in;
+	reg [8:0] freq_in;
 	reg [1:0] count;
 	reg [2:0] odd_idx;
 	reg [2:0] even_idx;
 	wire [95:0] initial_node;
-	reg [3:0] state;
+	reg [2:0] state;
 	reg [31:0] huff_tree [0:5];
 	reg [95:0] in_huff_tree;
 	wire [127:0] out_huff_tree;
 	wire [31:0] merged_node;
+	reg [8:0] encoded_value;
+	reg [8:0] encoded_mask;
+	reg done;
+	reg [23:0] character;
 	reg [2:0] encoded_value_h [0:5];
-	reg [2:0] n;
+	reg [2:0] a;
+	reg [2:0] b;
+	reg [2:0] c;
 	reg [2:0] encoded_value_l;
 	reg [2:0] encoded_value_r;
 	reg is_n_odd;
@@ -46,14 +47,17 @@ module huff_encoder (
 	);
 	always @(posedge clk) begin : huffman_enc
 		if (reset) begin
-			state <= 4'b0000;
-			n <= 3'd2;
+			state <= 3'b001;
+			a <= 3'd0;
+			b <= 3'b000;
+			c <= 3'b000;
+			done = 'b0;
 			begin : sv2v_autoblock_1
 				reg signed [31:0] i;
 				for (i = 0; i < 3; i = i + 1)
 					begin
+						encoded_value[(2 - i) * 3+:3] = 'b0;
 						encoded_mask[(2 - i) * 3+:3] = 'b0;
-						character[(2 - i) * 7+:7] = 'b0;
 						in_huff_tree[((2 - i) * 32) + 31-:9] = 'b0;
 						in_huff_tree[((2 - i) * 32) + 22-:4] = 'b0;
 						in_huff_tree[((2 - i) * 32) + 18] = 'b0;
@@ -61,38 +65,41 @@ module huff_encoder (
 						in_huff_tree[((2 - i) * 32) + 8-:9] = 'b0;
 					end
 			end
+			begin : sv2v_autoblock_2
+				reg signed [31:0] i;
+				for (i = 0; i < 6; i = i + 1)
+					begin
+						encoded_value_h[i] = 'b0;
+						huff_tree[i][31-:9] = 'b0;
+						huff_tree[i][22] = 1'b0;
+						huff_tree[i][3-:2] = 'b0;
+						huff_tree[i][21-:9] = 'b0;
+						huff_tree[i][12-:9] = 'b0;
+						huff_tree[i][1-:2] = 'b0;
+					end
+			end
+			io_out <= 'b0;
 		end
 		else
 			case (state)
-				4'b0000: begin
+				3'b001: begin
 					done = 'b0;
-					begin : sv2v_autoblock_2
-						reg signed [31:0] i;
-						for (i = 0; i < 6; i = i + 1)
-							begin
-								encoded_value_h[i] = 'b0;
-								huff_tree[i][31-:9] = 'b0;
-								huff_tree[i][22] = 1'b0;
-								huff_tree[i][3-:2] = 'b0;
-								huff_tree[i][21-:9] = 'b0;
-								huff_tree[i][12-:9] = 'b0;
-								huff_tree[i][1-:2] = 'b0;
-							end
-					end
-					state <= 4'b0001;
+					data_in[(2 - c) * 8+:8] <= io_in[7:0];
+					freq_in[(2 - c) * 3+:3] <= io_in[10:8];
+					c <= (io_in[11] ? c + 1'b1 : 'b0);
+					state <= (c == 2 ? 3'b010 : 3'b001);
 				end
-				4'b0001: state <= 4'b0010;
-				4'b0010: begin
+				3'b010: begin
 					count = 3;
 					begin : sv2v_autoblock_3
 						reg signed [31:0] i;
 						for (i = 0; i < 3; i = i + 1)
 							in_huff_tree[(2 - i) * 32+:32] = initial_node[(2 - i) * 32+:32];
 					end
-					state <= 4'b0011;
+					state <= 3'b011;
 				end
-				4'b0011: state <= 4'b0100;
-				4'b0100: begin
+				3'b011: state <= 3'b100;
+				3'b100: begin
 					in_huff_tree[64+:32] = merged_node;
 					in_huff_tree[0+:64] = out_huff_tree[0+:64];
 					count = count - 1'b1;
@@ -111,11 +118,11 @@ module huff_encoder (
 					huff_tree[1][21-:9] = out_huff_tree[113-:9];
 					huff_tree[1][12-:9] = out_huff_tree[104-:9];
 					if (!(count[0] | count[1]))
-						state <= 4'b0101;
+						state <= 3'b101;
 					else
-						state <= 4'b0011;
+						state <= 3'b011;
 				end
-				4'b0101: begin
+				3'b101: begin
 					begin : sv2v_autoblock_4
 						reg signed [31:0] l;
 						for (l = 5; l > 1; l = l - 1)
@@ -126,43 +133,50 @@ module huff_encoder (
 										huff_tree[l][3-:2] = n;
 							end
 					end
-					state <= 4'b0110;
-				end
-				4'b0110: begin
-					for (n = 2; n < 6; n = n + 1)
-						huff_tree[n][1-:2] = huff_tree[huff_tree[n][3-:2]][1-:2] + 1'b1;
-					state <= 4'b0111;
-				end
-				4'b0111: begin
-					for (n = 2; n < 6; n = n + 1)
-						begin
-							encoded_value_l = encoded_value_h[huff_tree[n][3-:2]] << 1'b1;
-							encoded_value_r = (encoded_value_h[huff_tree[n][3-:2]] << 1'b1) | 1'b1;
-							is_n_odd = n[0];
-							if (huff_tree[n][3-:2] != 1'b1)
-								encoded_value_h[n] = (is_n_odd ? encoded_value_l : encoded_value_r);
-							else if (huff_tree[n][3-:2] == 1'b1)
-								encoded_value_h[n][0] = (is_n_odd ? 1'b0 : 1'b1);
-						end
-					state <= 4'b1000;
-				end
-				4'b1000: begin
 					begin : sv2v_autoblock_6
+						reg signed [31:0] n;
+						for (n = 2; n < 6; n = n + 1)
+							huff_tree[n][1-:2] = huff_tree[huff_tree[n][3-:2]][1-:2] + 1'b1;
+					end
+					state <= 3'b110;
+				end
+				3'b110: begin
+					begin : sv2v_autoblock_7
+						reg signed [31:0] n;
+						for (n = 2; n < 6; n = n + 1)
+							begin
+								encoded_value_l = encoded_value_h[huff_tree[n][3-:2]] << 1'b1;
+								encoded_value_r = (encoded_value_h[huff_tree[n][3-:2]] << 1'b1) | 1'b1;
+								is_n_odd = n[0];
+								if (huff_tree[n][3-:2] != 1'b1)
+									encoded_value_h[n] = (is_n_odd ? encoded_value_l : encoded_value_r);
+								else if (huff_tree[n][3-:2] == 1'b1)
+									encoded_value_h[n][0] = (is_n_odd ? 1'b0 : 1'b1);
+							end
+					end
+					begin : sv2v_autoblock_8
 						integer i;
-						for (i = 2; i >= 0; i = i - 1)
-							begin : sv2v_autoblock_7
+						for (i = 0; i <= 2; i = i + 1)
+							begin : sv2v_autoblock_9
 								reg signed [31:0] n;
 								for (n = 1; n < 6; n = n + 1)
-									if (huff_tree[n][31-:9] == data_in[i * 8+:8]) begin
+									if (huff_tree[n][31-:9] == data_in[(2 - i) * 8+:8]) begin
 										encoded_mask[(2 - i) * 3+:3] = (1'b1 << huff_tree[n][1-:2]) - 1'b1;
-										character[(2 - i) * 7+:7] = huff_tree[n][31-:9];
 										encoded_value[(2 - i) * 3+:3] = encoded_value_h[n];
+										character[(2 - i) * 8+:8] = huff_tree[n][31-:9];
 									end
 							end
 					end
-					done = 1'b1;
+					state <= 3'b111;
 				end
-				default: state <= 4'b0000;
+				3'b111: begin
+					done = 1'b1;
+					io_out[8:0] <= (b[0] == 1'b0 ? {done, character[(2 - a) * 8+:8]} : {done, 2'b00, encoded_mask[(2 - a) * 3+:3], encoded_value[(2 - a) * 3+:3]});
+					b <= b + 1'b1;
+					a <= (b[0] == 1'b1 ? a + 1 : a);
+					state <= (a == 3 ? 3'b001 : 3'b111);
+				end
+				default: state <= 3'b001;
 			endcase
 	end
 endmodule
